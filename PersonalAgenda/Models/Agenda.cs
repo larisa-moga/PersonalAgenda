31c52dc753aef6de7903177997221d090f735a8a
LARISA-IOANA MOGA <larisa.moga@stud.ubbcluj.ro>
2020-12-10 09:32:55 +0200

    Add project files.

---
+++ PersonalAgenda/Models/Agenda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalAgenda.Models
+{
+    public class Agenda
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
